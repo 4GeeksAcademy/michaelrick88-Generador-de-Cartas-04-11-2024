02ad067ab05dae98330b9a7f0aafb1082df0d931
Michaelrick88 <michaelrick88@hotmail.com>
2024-11-06 19:20:33 +0000

    [michaelrick88 realice el generador de gartas ]
    usando ya los archivos ya existentes de css,js y html realice el generador de cartas aleatorias borrando el contenido ya existente

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generadorDeCarta();
 };
+
+function generadorDeCarta() {
+  let simbolos = ["♦", "♥", "♠", "♣"];
+  let numeros = ["A", "J", "Q", "K", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  let randonSimbolos = simbolos[Math.floor(Math.random() * simbolos.length)];
+  let randonNumeros = numeros[Math.floor(Math.random() * numeros.length)];
+
+  let color;
+  if (randonSimbolos === "♦" || randonSimbolos === "♥") {
+    color = "red";
+  } else {
+    color = "black";
+  }
+
+  document.getElementById("simbolo").innerText = randonSimbolos;
+  document.getElementById("simbolo").style.color = color;
+
+  document.getElementById("simbolo2").innerText = randonSimbolos;
+  document.getElementById("simbolo2").style.color = color;
+
+  document.getElementById("numero").innerText = randonNumeros;
+  document.getElementById("numero").style.color = color;
+}
